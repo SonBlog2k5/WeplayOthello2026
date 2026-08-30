5f792f984c30a415d8301859a3f2b54fcfde61b0
SonBlog2k5 <94066046+SonBlog2k5@users.noreply.github.com>
2026-08-30 23:53:20 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-
-android { namespace = "com.weplay.othello"; compileSdk = 35
-    defaultConfig { applicationId = "com.weplay.othello"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
